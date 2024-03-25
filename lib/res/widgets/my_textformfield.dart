6b276a5562d87120396ca27ceed2fcc717166201
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-25 23:07:51 +0500

    my custom textFormField widget

---
+++ lib/res/widgets/my_textformfield.dart
@@ -0,0 +1,63 @@
+import 'package:attendancemanagementsystem/res/my_colors/my_colors.dart';
+import 'package:flutter/material.dart';
+
+class MyTextFormField extends StatelessWidget {
+  final String? hintText;
+  final Color? color;
+  final double? fontSize;
+  final FontWeight? fontWeight;
+  final TextAlign? textAlign;
+  final Color? fillColor;
+  final BorderSide border;
+  final BorderSide enabledBorder;
+  final BorderSide focusedBorderSide;
+  final String? Function(String?)? validator;
+  final TextEditingController? controller;
+  final TextInputType? keyboardType;
+  final bool obscureText;
+
+  const MyTextFormField(
+      {super.key,
+      required this.hintText,
+      this.color,
+      this.fontSize,
+      this.fontWeight,
+      this.textAlign,
+      this.fillColor,
+      this.border = const BorderSide(color: Colors.grey, width: 2.0),
+      this.enabledBorder = const BorderSide(color: Colors.grey, width: 2.0),
+      this.focusedBorderSide = const BorderSide(color: Color(0xFF616161), width: 2.0),
+      this.validator,
+      required this.controller,
+      this.keyboardType,
+      this.obscureText = false});
+
+  @override
+  Widget build(BuildContext context) {
+    return TextFormField(
+      controller: controller,
+      keyboardType: keyboardType,
+      obscureText: obscureText,
+      decoration: InputDecoration(
+          hintText: hintText,
+          hintStyle: TextStyle(
+            color: color,
+            fontSize: fontSize,
+            fontWeight: fontWeight,
+          ),
+          filled: true,
+          fillColor: fillColor,
+        border: OutlineInputBorder(
+          borderSide: border
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderSide: enabledBorder
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderSide: focusedBorderSide
+        ),
+      ),
+      validator: validator,
+    );
+  }
+}
