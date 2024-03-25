1a6362e8a3c7eb9f873ebe4ee412b3efb2e3ed41
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-25 23:06:35 +0500

    for constant resourses

---
+++ lib/res/constant/constant.dart
@@ -0,0 +1,17 @@
+
+
+
+import 'package:attendancemanagementsystem/res/my_colors/my_colors.dart';
+import 'package:fluttertoast/fluttertoast.dart';
+
+class Constant {
+
+  void toastMessage(String message) {
+    Fluttertoast.showToast(
+        msg: message,
+      backgroundColor: MyColors.lightgrayColor,
+      textColor: MyColors.blackColor,
+    );
+  }
+
+}
