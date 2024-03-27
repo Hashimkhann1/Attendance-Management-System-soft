4d6d4826617774b300c7fa237f611d020ec7d4aa
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-27 23:39:46 +0500

    profile is completed

--- lib/view/profile_view/profile_view.dart
+++ lib/view/profile_view/profile_view.dart
@@ -2,12 +2,16 @@ import 'package:attendancemanagementsystem/res/my_colors/my_colors.dart';
 import 'package:attendancemanagementsystem/res/widgets/my_profile_card.dart';
 import 'package:attendancemanagementsystem/res/widgets/my_text.dart';
 import 'package:attendancemanagementsystem/view_model/auth_view_model/auth_view_model.dart';
+import 'package:attendancemanagementsystem/view_model/getx/logedIn_user_data_getx/logedIn_user_data.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:get/get.dart';
 
-class AdminProfileView extends StatelessWidget {
-  AdminProfileView({super.key});
+class ProfileView extends StatelessWidget {
+  ProfileView({super.key});
 
   final AuthViewModel authViewModel = AuthViewModel();
+  final LogedInUserDataGetx logedInUserDataGetx = Get.put(LogedInUserDataGetx());
 
   @override
   Widget build(BuildContext context) {
@@ -31,15 +35,33 @@ class AdminProfileView extends StatelessWidget {
       body: Column(
         mainAxisAlignment: MainAxisAlignment.center,
         children: [
-          CircleAvatar(
-            radius: 58,
+          Stack(
+            clipBehavior: Clip.none,
+            children: [
+              CircleAvatar(
+                radius: 66,
+                backgroundColor: MyColors.lightBlackColor,
+              ),
+              Positioned(
+                bottom: 2,
+                right: 8,
+                child: Container(
+                  padding: EdgeInsets.all(4),
+                  decoration: BoxDecoration(
+                    color: MyColors.lightgrayColor,
+                    shape: BoxShape.circle
+                  ),
+                  child: Icon(Icons.camera_alt,color: MyColors.lightBlackColor,),
+                ),
+              )
+            ],
           ),
           SizedBox(
             height: height * 0.06,
           ),
-          MyProfileCard(title: "M Hashim"),
+          MyProfileCard(title: logedInUserDataGetx.userDataList[0].userName.toString()),
           MyProfileCard(
-            title: "hashim@gmail.com",
+            title: logedInUserDataGetx.userDataList[0].userEmail.toString(),
             icon: Icons.email_outlined,
           ),
           InkWell(
