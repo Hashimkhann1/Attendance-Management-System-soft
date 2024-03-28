3fddedbc18f5235b771bf74d8fda0478bf92db69
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-28 23:00:28 +0500

    all colors are save in this file

--- lib/res/my_colors/my_colors.dart
+++ lib/res/my_colors/my_colors.dart
@@ -9,6 +9,7 @@ class MyColors {
   static const whiteColor = Colors.white;
   static const grayColor = Colors.grey;
   static const blackColor = Colors.black;
+  static const lightBlackColor = Color(0xFF4c4e57);
   static final midLightgrayColor = Colors.grey.shade600;
   static final lightgrayColor = Color(0xFFBDBDBD);
 
