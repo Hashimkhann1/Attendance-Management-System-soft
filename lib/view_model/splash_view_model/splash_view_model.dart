f289f20178faef30ea0ac023b664816f61c57f9d
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-29 22:33:25 +0500

    splash view and view model is compleetd and call method to gget all data of user

--- lib/view_model/splash_view_model/splash_view_model.dart
+++ lib/view_model/splash_view_model/splash_view_model.dart
@@ -3,11 +3,28 @@
 
 import 'dart:async';
 
+import 'package:attendancemanagementsystem/view/admin_view/all_students_view/all_students_view.dart';
+import 'package:attendancemanagementsystem/view/auth_view/sign_in_view/sign_in_view.dart';
+import 'package:attendancemanagementsystem/view/bottom_navigator_view/bottom_navigator_view.dart';
+import 'package:attendancemanagementsystem/view_model/get_logedin_user_data/get_logedin_user_data.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
+
 class SplashViewModel {
 
-  void splashTime() {
-    Timer.periodic(const Duration(seconds: 3), (timer) {
-      print("Time is Done");
+  final _auth = FirebaseAuth.instance;
+
+  final GetLogedInUserData getLogedInUserData = GetLogedInUserData();
+
+  Future<void> splashTime(BuildContext context) async {
+    Future.delayed(Duration(seconds: 3) , () {
+      if(_auth.currentUser != null){
+        getLogedInUserData.getUserData();
+        Navigator.push(context, MaterialPageRoute(builder: (context) => BottomNavigatorView()));
+      }else{
+        Navigator.push(context, MaterialPageRoute(builder: (context) => SignInView()));
+      }
     });
   }
 
