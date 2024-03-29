1a897a0bf0a2b4a71a68d3669f311854cf64e08c
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-29 22:32:33 +0500

    for storing user data in list is in this this file

---
+++ lib/view_model/getx/logedIn_user_data_getx/logedIn_user_data.dart
@@ -0,0 +1,13 @@
+
+
+
+import 'package:get/get.dart';
+import 'package:get/get_rx/get_rx.dart';
+import 'package:get/get_rx/src/rx_types/rx_types.dart';
+
+class LogedInUserDataGetx extends GetxController {
+
+  /////// all user data is added to this list
+  RxList userDataList = [].obs;
+
+}
