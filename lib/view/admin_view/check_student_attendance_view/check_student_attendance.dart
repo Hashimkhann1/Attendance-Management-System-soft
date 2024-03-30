46f566518a98b1d797a42a61843ca0ab13b93c12
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-30 23:13:12 +0500

    completed

--- lib/view/admin_view/check_student_attendance_view/check_student_attendance.dart
+++ lib/view/admin_view/check_student_attendance_view/check_student_attendance.dart
@@ -2,6 +2,7 @@ import 'package:attendancemanagementsystem/res/my_colors/my_colors.dart';
 import 'package:attendancemanagementsystem/res/widgets/my_text.dart';
 import 'package:attendancemanagementsystem/view/admin_view/check_student_attendance_view/student_attendance_graph/student_attendance_graph.dart';
 import 'package:attendancemanagementsystem/view_model/attendance_view_model/attendance_graph_View_model/attendance_graph_View_model.dart';
+import 'package:attendancemanagementsystem/view_model/attendance_view_model/attendance_view_model.dart';
 import 'package:attendancemanagementsystem/view_model/getx/logedIn_user_data_getx/logedIn_user_data.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
@@ -10,16 +11,18 @@ import 'package:get/get.dart';
 class CheckStudentAttendanceView extends StatefulWidget {
 
   final String studentId;
-  CheckStudentAttendanceView({super.key,required this.studentId});
+  final DateTime userRegisterDate;
+
+  CheckStudentAttendanceView({super.key,required this.studentId , required this.userRegisterDate});
 
   @override
-  State<CheckStudentAttendanceView> createState() => _CheckStudentAttendanceViewState();
-}
+  State<CheckStudentAttendanceView> createState() => _CheckStudentAttendanceViewState();}
 
 class _CheckStudentAttendanceViewState extends State<CheckStudentAttendanceView> {
 
   final LogedInUserDataGetx logedInUserDataGetx = Get.put(LogedInUserDataGetx());
   final AttendanceGraphViewModel attendanceGraphViewModel = Get.put(AttendanceGraphViewModel());
+  final AttendanceViewModel attendanceViewModel = Get.put(AttendanceViewModel());
 
   @override
   void initState() {
@@ -78,7 +81,7 @@ class _CheckStudentAttendanceViewState extends State<CheckStudentAttendanceView>
                     ////////////////////////////////////////////////
                     /////////// student attendance graph ///////////
                     ////////////////////////////////////////////////
-                    StudenAttendanceGraph(studentId: widget.studentId,),
+                    StudenAttendanceGraph(studentId: widget.studentId,userRegisterDate: widget.userRegisterDate,),
 
                     SizedBox(
                       height: height * 0.01,
@@ -101,9 +104,14 @@ class _CheckStudentAttendanceViewState extends State<CheckStudentAttendanceView>
                                 children: [
                                   Row(
                                     children: [
-                                      logedInUserDataGetx.userDataList[0].userType != 'admin' ? SizedBox() : Icon(
-                                        Icons.delete,
-                                        color: MyColors.whiteColor,
+                                      logedInUserDataGetx.userDataList[0].userType != 'admin' ? SizedBox() : InkWell(
+                                        onTap: (){
+                                          attendanceViewModel.deleteAttendance(context, widget.studentId, snapshot.data!.docs[index].id.toString());
+                                        },
+                                        child: Icon(
+                                          Icons.delete,
+                                          color: MyColors.whiteColor,
+                                        ),
                                       ),
                                       SizedBox(
                                         width: width * 0.02,
