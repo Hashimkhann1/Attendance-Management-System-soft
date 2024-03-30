638f3c0ef44d205ff1007920afb67b8d4024012f
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-03-30 23:15:51 +0500

    completed

--- lib/view/student_view/student_attandance_view/student_attandance_view.dart
+++ lib/view/student_view/student_attandance_view/student_attandance_view.dart
@@ -3,6 +3,7 @@ import 'package:attendancemanagementsystem/res/widgets/my_text.dart';
 import 'package:attendancemanagementsystem/res/widgets/my_text_button.dart';
 import 'package:attendancemanagementsystem/view/admin_view/check_student_attendance_view/check_student_attendance.dart';
 import 'package:attendancemanagementsystem/view_model/attendance_view_model/attendance_view_model.dart';
+import 'package:attendancemanagementsystem/view_model/getx/logedIn_user_data_getx/logedIn_user_data.dart';
 import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
@@ -18,6 +19,9 @@ class StudentAttendanceView extends StatefulWidget {
 class _StudentAttendanceViewState extends State<StudentAttendanceView> {
   final AttendanceViewModel attendanceViewModel =
       Get.put(AttendanceViewModel());
+
+  final LogedInUserDataGetx logedInUserDataGetx =
+      Get.put(LogedInUserDataGetx());
 
   final _auth = FirebaseAuth.instance;
 
@@ -36,82 +40,88 @@ class _StudentAttendanceViewState extends State<StudentAttendanceView> {
     return Scaffold(
         backgroundColor: MyColors.blackColor,
         appBar: AppBar(
-                title: MyText(
-                  title: "Mark Attendance",
-                  fontWeight: FontWeight.bold,
-                  color: MyColors.whiteColor,
-                ),
-                automaticallyImplyLeading: false,
-                centerTitle: true,
-                backgroundColor: MyColors.blackColor,
-                shadowColor: MyColors.whiteColor,
-                elevation: 16,
-              ),
+          title: MyText(
+            title: "Mark Attendance",
+            fontWeight: FontWeight.bold,
+            color: MyColors.whiteColor,
+          ),
+          automaticallyImplyLeading: false,
+          centerTitle: true,
+          backgroundColor: MyColors.blackColor,
+          shadowColor: MyColors.whiteColor,
+          elevation: 16,
+        ),
         body: Obx(() {
           return attendanceViewModel.isTodayAttendanceExist.value == 'loading'
               ? Center(
                   child: CircularProgressIndicator(
                   color: MyColors.whiteColor,
-                )) : PopScope(
-                      canPop: false,
-                      onPopInvoked: (didPop) {
-                        SystemNavigator.pop();
-                      },
-                      child: Column(
-                        children: [
-                          SizedBox(
-                            height: height * 0.06,
-                          ),
-                          MyTextButton(
-                            title: "Mark present",
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            textColor: MyColors.whiteColor,
-                            onPressed: () {
-                              attendanceViewModel.showAttendanceDialog(
-                                  context, 'present');
-                            },
-                            width: width,
-                            height: height * 0.10,
-                            backgroundColor: MyColors.lightBlackColor,
-                          ),
-                          SizedBox(
-                            height: height * 0.02,
-                          ),
-                          MyTextButton(
-                            title: "Mark leave",
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            textColor: MyColors.whiteColor,
-                            onPressed: () {
-                              attendanceViewModel.showAttendanceDialog(
-                                  context, 'leave');
-                            },
-                            width: width,
-                            height: height * 0.10,
-                            backgroundColor: MyColors.lightBlackColor,
-                          ),
-                          SizedBox(
-                            height: height * 0.02,
-                          ),
-                          MyTextButton(
-                            title: "View Attendance",
-                            fontSize: 20,
-                            fontWeight: FontWeight.w500,
-                            textColor: MyColors.whiteColor,
-                            onPressed: () {
-                              Navigator.push(
-                                  context,
-                                  MaterialPageRoute(
-                                      builder: (context) =>
-                                          CheckStudentAttendanceView(studentId: _auth.currentUser!.uid.toString(),)));
-                            },
-                            width: width,
-                            height: height * 0.10,
-                            backgroundColor: MyColors.lightBlackColor,
-                          ),
-                        ],
-                      ));
+                ))
+              : PopScope(
+                  canPop: false,
+                  onPopInvoked: (didPop) {
+                    SystemNavigator.pop();
+                  },
+                  child: Column(
+                    children: [
+                      SizedBox(
+                        height: height * 0.06,
+                      ),
+                      MyTextButton(
+                        title: "Mark present",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        textColor: MyColors.whiteColor,
+                        onPressed: () {
+                          attendanceViewModel.showAttendanceDialog(
+                              context, 'present');
+                        },
+                        width: width,
+                        height: height * 0.10,
+                        backgroundColor: MyColors.lightBlackColor,
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      MyTextButton(
+                        title: "Mark leave",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        textColor: MyColors.whiteColor,
+                        onPressed: () {
+                          attendanceViewModel.showAttendanceDialog(
+                              context, 'leave');
+                        },
+                        width: width,
+                        height: height * 0.10,
+                        backgroundColor: MyColors.lightBlackColor,
+                      ),
+                      SizedBox(
+                        height: height * 0.02,
+                      ),
+                      MyTextButton(
+                        title: "View Attendance",
+                        fontSize: 20,
+                        fontWeight: FontWeight.w500,
+                        textColor: MyColors.whiteColor,
+                        onPressed: () {
+                          Navigator.push(
+                              context,
+                              MaterialPageRoute(
+                                  builder: (context) =>
+                                      CheckStudentAttendanceView(
+                                        studentId:
+                                            _auth.currentUser!.uid.toString(),
+                                        userRegisterDate: logedInUserDataGetx
+                                            .userDataList[0].registerData.toDate(),
+                                      )));
+                        },
+                        width: width,
+                        height: height * 0.10,
+                        backgroundColor: MyColors.lightBlackColor,
+                      ),
+                    ],
+                  ));
         }));
   }
 }
