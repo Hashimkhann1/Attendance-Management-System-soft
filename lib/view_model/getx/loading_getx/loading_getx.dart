005bc920e8b0a18dab7bd0011ab7a5f0ee98d6d9
Hashim khan <84334197+Hashimkhann1@users.noreply.github.com>
2024-04-01 22:55:47 +0500

    loading for utton getx

---
+++ lib/view_model/getx/loading_getx/loading_getx.dart
@@ -0,0 +1,14 @@
+
+
+
+import 'package:get/get.dart';
+
+class LoadingGetx extends GetxController{
+
+  RxBool isLoading = false.obs;
+
+  void setLoading() {
+    isLoading.value = !isLoading.value;
+  }
+
+}
